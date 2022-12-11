d4f18378d381fece7cfd1970bf14b9f901849df3
rtungol <rarrtungol@gmail.com>
2022-12-11 17:22:04 -0500

    adding components and pages

---
+++ src/components/BlogForm/BlogForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BlogForm() {
+  return (
+    <div>BlogForm</div>
+  )
+}
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,12 @@
-
-import './App.css';
+import "./App.css";
+import Home from "../Home/Home";
+import Blog from "../Blog/Blog";
 
 function App() {
   return (
-    <div className="App">Portfolio
+    <div className="App">
+      <Home />
+      <Blog />
     </div>
   );
 }
