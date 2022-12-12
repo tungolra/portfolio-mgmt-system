a71406e24063785f8e86cb479df470ce87ea08e0
rtungol <rarrtungol@gmail.com>
2022-12-12 18:07:07 -0500

    upload images for skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,13 +4,18 @@ import * as seeds from "../../seed.js";
 
 export default function Skills() {
   return (
-    <section className="skills-container">
-      {seeds.skills.map((skill) => (
-        <div className="skill-item">
-          <div>{skill.skill}</div>
-          <img src={skill.img} alt={skill.name} />
-        </div>
-      ))}
-    </section>
+    <>
+      <label> Skills </label>
+      <section className="skills-container">
+        {seeds.skills.map((skill) => (
+          <div className="skill-item">
+            <div>{skill.skill}</div>
+            <img className="tech-icons" src={skill.img} alt={skill.skill} />
+            {/* <SVG src={skill.img}/> */}
+            {console.log(skill.img)}
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
