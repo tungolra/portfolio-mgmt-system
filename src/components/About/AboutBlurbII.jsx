6d084127a5d5c90f392ddbf1082527953b84f623
rtungol <rarrtungol@gmail.com>
2022-12-30 21:07:15 -0500

    baseline

---
+++ src/components/About/AboutBlurbII.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function AboutBlurbII() {
+  return (
+    <>
+      <p>
+        This simple website was created with React. Now, that may seem overkill,
+        but I didn't want this to be "one-and-done" once I had a presentable
+        portfolio page. Keeping in mind that I'll continue to develop more
+        projects in the future and the need to track my learning experience,
+        I've set up the foundation through this portfolio to do more. Now that
+        this website is posted, I am in the process of building out RESTful APIs
+        to add new projects and journal entries through my website.
+      </p>
+      <p>
+        As a developer early into his career, I prioritize organization,
+        step-wise project-building, and test-driven development. I aim to write
+        clean and readable code, hitting the MVP, and Agile workflows for
+        consistent productivity. In this portfolio project - turned portfolio
+        management system - turned blog - I decided that React would be a great
+        use case to execute these features.
+      </p>
+      <p>
+        I am pulled towards any opportunity involving MERN as a tech stack. But
+        that won't stop me from learning new technologies! For me, part of the
+        appeal of programming is it's continuous evolution. Developing
+        proficencies is the game. Becoming proficient in development is the
+        goal.
+      </p>
+      <p>
+        I am meticulous when it comes to the overall front-end design and user
+        experience - of course, within given time constraints. Having completed
+        my full-stack software engineering program with General Assembly, I gave
+        myself 5-days to create this portfolio to continue creating more
+        applications.
+      </p>
+      <p>
+        For now, I've created a responsive webpage by taking a mobile-first
+        approach. I aimed to showcase my CSS knowledge instead of relying on
+        styling frameworks such as Material UI. I used native React elements
+        where necessary, but kept to HTML to demonstrate this fundamental skill.
+      </p>
+      <p>
+        IDEs and CLI are a programmer's bread and butter. That's how I decided
+        on the theme for this portfolio. If you're viewing this on desktop,
+        you'll have noticed the Matrix-esque background on the edges of the
+        page. Running binaries to signal the fact that this is a "programmer's
+        website" may be cliche, but it just fit so well with the theme! How can
+        I resist?
+      </p>
+      <p>
+        Whenever I put down one project, I'm immediately eager to get my feet
+        wet in another. I'd love to know what you'll be building next! Please
+        get in touch and let's talk! Anyways, feel free to have a look around.
+        Check out the repo for this project. And thanks for visiting! Look
+        forward to hearing from you.
+      </p>
+      <p>- Ralph</p>
+    </>
+  );
+}
