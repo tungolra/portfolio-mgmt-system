ba71e40e53bc174125edc272acf9a56bfd7b8b67
rtungol <rarrtungol@gmail.com>
2023-01-01 22:55:24 -0500

    reconfigure slidein nav component into navbar and projectslist + labslist into allworkpage component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,47 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-import SlideInNav from "../SlideInNav/SlideInNav";
 import * as userService from "../../utilities/users-service";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { BiMessageDetail } from "react-icons/bi";
 import { AiOutlineAppstore, AiOutlineHome } from "react-icons/ai";
 import { BsLightning } from "react-icons/bs";
+
+import NewEntry from "../../pages/NewEntry/NewEntry";
+
+function SlideInNav({ modalOpened, handleClick, user }) {
+  return (
+    <>
+      {modalOpened ? (
+        <div className="overlay">
+          <div className="overlay-content">
+            <Link to="/home" onClick={handleClick}>
+              Home
+            </Link>
+            <a href="/#about" onClick={handleClick}>
+              About
+            </a>
+            <a href="/#projects" onClick={handleClick}>
+              Projects
+            </a>
+            <a href="/#skills" onClick={handleClick}>
+              Skills
+            </a>
+            {/* <a href="#">Blog</a> */}
+            <a href="/#footer" onClick={handleClick}>
+              Contact
+            </a>
+            {user ? <Link to="/entry" onClick={handleClick} >New Entry</Link> : ""}
+            {user ? <Link to="/new-project" onClick={handleClick} >Add Project</Link> : ""}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
 
 export default function NavBar({ setUser, user }) {
   const [modalOpened, setModalOpened] = useState(false);
--- src/pages/AllWorkPage/AllWorkPage.jsx
+++ src/pages/AllWorkPage/AllWorkPage.jsx
@@ -1,8 +1,76 @@
 import React from "react";
-import LabsList from "../../components/LabsList/LabsList";
-import ProjectsList from "../../components/ProjectsList/ProjectsList";
 import "./AllWorkPage.css";
+import { Link } from "react-router-dom";
 
+function ProjectsList({ seed, user }) {
+  return (
+    <div className="list-container">
+      <h2>Projects List</h2>
+      <table>
+        <thead>
+          <th>Project Name</th>
+          <th>Repo</th>
+          <th>Site</th>
+          <th>Status</th>
+          {user ? <th>Update</th> : ""}
+        </thead>
+        <tbody>
+          {seed.projects?.map((project) => (
+            <>
+              <tr>
+                <td>{project?.name}</td>
+                <td>
+                  <a href={project?.repo}>Repo</a>
+                </td>
+                <td>
+                  <a href={project?.site}>Site</a>
+                </td>
+                <td>Offline</td>
+                {/* link to update page */}
+                {user ? <td><Link to={`update/${project?.name}`}> Update</Link></td> : ""}
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function LabsList({seed, user}) {
+  return (
+    <div className="list-container">
+      <h2>Labs List</h2>
+      <table>
+        <thead>
+          <th>Lab Name</th>
+          <th>Repo</th>
+          <th>Site</th>
+          <th>Status</th>
+          {user ? <th>Update</th> : ""}
+        </thead>
+        <tbody>
+          {seed.projects?.map((project) => (
+            <>
+              <tr>
+                <td>{project?.name}</td>
+                <td>
+                  <a href={project?.repo}>Repo</a>
+                </td>
+                <td>
+                  <a href={project?.site}>Site</a>
+                </td>
+                <td>Offline</td>
+                {/* link to update page */}
+                {/* {user ? <td>Update</td> : ""} */}
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 export default function AllWorkPage({ seed, user }) {
   return (
     <div className="all-work-container">
