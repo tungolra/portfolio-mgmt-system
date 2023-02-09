4e4cfa8f18b4b6ba76e7fa86f13b8c2d78973bc4
rtungol <rarrtungol@gmail.com>
2023-02-09 10:41:36 -0500

    remove proxy for deployment

--- src/components/Projects/ProjectsCollage.jsx
+++ src/components/Projects/ProjectsCollage.jsx
@@ -15,8 +15,6 @@ export default function ProjectsCollage() {
     header: "Featured",
   });
 
-  console.log(seed.countBySubtype)
-
   const categoryButtonsByType = () => {
     const categories = [
       { category: "Featured", filter: featuredProjects() },
